0fb0cf407804e0bac8ce2aef3d3695ec5886f0c3
Christopher <kriss17388+dev@gmail.com>
2023-07-22 11:55:01 +0100

    working api

---
+++ main.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import requests
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi import Response
+import os
+
+load_dotenv()
+
+key = os.environ["DRF_API_KEY"]
+
+app = FastAPI()
+
+@app.get("/{email}")
+async def root(email):
+    print("hiya")
+    # print("Email is ", email)
+    # print("Key is ", key)
+    r = requests.get(f'https://api.songcards.io/retrieve_artist_user/{email}', headers={'Authorization': f'Api-Key {key}'})
+    text = r.text
+    print(text)
+    return Response(content=text, media_type="application/json")
+
